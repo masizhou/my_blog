a3a48858e7976ac8b3bcb99fa2a0441540a01d15
masizhou <1907609069@qq.com>
2021-03-20 17:35:25 +0800

    修复了bug

--- my_blog/users/admin.py
+++ my_blog/users/admin.py
@@ -3,8 +3,8 @@ from users.models import User
 
 # Register your models here.
 
-admin.site.site_header = 'BBS后台管理系统'
-admin.site.site_title = 'BBS后台'
+admin.site.site_header = '个人博客后台管理系统'
+admin.site.site_title = 'B个人博客后台'
 admin.site.index_title = '后台管理'
 
 # 注册模型
